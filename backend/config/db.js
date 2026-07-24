e605d54d6802b3148ecbd67044139f0b8104d6a2
Armando Romero <162987396+mandoromero@users.noreply.github.com>
2026-07-24 14:16:52 -0700

    backend is working

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,30 +1,38 @@
 import pkg from "pg";
 import dotenv from "dotenv";
-
+ 
 /* =========================
-   ENV CONFIG
+   ENV CONFIG6
 ========================= */
 dotenv.config();
-
+ 
 const { Pool } = pkg;
+
+console.log({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
 
 /* =========================
    POSTGRES CONNECTION POOL
 ========================= */
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT) || 5432,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-
-  // Connection pool settings
-  max: 10,
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000,
+   host: process.env.DB_HOST,
+   port: Number(process.env.DB_PORT) || 5432,
+   user: process.env.DB_USER,
+   password: process.env.DB_PASSWORD,
+   database: process.env.DB_NAME,
+   
+   max: 10,
+   idleTimeoutMillis: 30000,
+   connectionTimeoutMillis: 2000,
 });
 
 /* =========================
    EXPORT POOL
 ========================= */
+
 export default pool;
