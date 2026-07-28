a37ddf06582e250db6adc8ad8b1e834d6c42b460
Armando Romero <162987396+mandoromero@users.noreply.github.com>
2026-07-28 13:10:28 -0700

    commit

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -6,7 +6,10 @@ import {
   forgotPassword,
   resetPassword,
   checkEmail,
+  getProfile,
 } from "../controllers/auth.controller.js";
+
+import { protect } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
@@ -17,6 +20,11 @@ console.log("auth.routes loaded");
 router.post("/check-email", checkEmail);
 router.post("/signup", signup);
 router.post("/login", login);
+
+console.log("✅ Profile route registered");
+
+router.get("/profile", protect, getProfile);
+
 router.post("/forgot-password", forgotPassword);
 router.post("/reset-password/:token", resetPassword);
 
